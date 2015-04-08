8a36c1ab4f42e34140ce5aa4f8a089a25f6213aa
squeez6009 <scott.jean@gmail.com>
2015-04-08 14:52:40 -0400

    Updating again

--- lab5.c
+++ lab5.c
@@ -1,4 +1,4 @@
-/** main.c: Main string suite for lab4_lib.c functions
+/*
 *
 * Name: Scott Jean
 * Email: sajean@purdue.edu
@@ -13,6 +13,7 @@
 /* Includes */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 #define FIRSTNAME 15
@@ -21,7 +22,7 @@
 #define AGE 2
 
 /* Function Prototypes */
-void create_list_no_nodes();
+
 void create_list_node();
 void insert_front();
 void insert_middle();
@@ -31,6 +32,7 @@ void delete_middle();
 void delete_end();
 void traverse();
 void look_up_by_index();
+void collect_data();
 void display_list();
 
 struct StudentInfo{
@@ -51,106 +53,106 @@ int user_choice;
   do {  
     printf("\n\n\nPlease Choose From the following Menu\n");
     printf("=====================================\n");
-    printf("\n1) Create a list with no nodes.\n");
-    printf("\n2) Create a list with a single node.\n");
-    printf("\n3) Insert node at front of list.\n");
-    printf("\n4) Insert node in middle of list.\n");
-    printf("\n5) Insert node at end of list.\n");
-    printf("\n6) Delete first node of list.\n");
-    printf("\n7) Delete middle node of list.\n");
-    printf("\n8) Delete node at end of list.\n");
-    printf("\n9) Traverse list.\n");
-    printf("\n10) Look up node by index.\n");
-    printf("\n11) Exit\n\n");
+    printf("\n1) Create a list with a single node.\n");
+    printf("\n2) Insert node at front of list.\n");
+    printf("\n3) Insert node in middle of list.\n");
+    printf("\n4) Insert node at end of list.\n");
+    printf("\n5) Delete first node of list.\n");
+    printf("\n6) Delete middle node of list.\n");
+    printf("\n7) Delete node at end of list.\n");
+    printf("\n8) Traverse list.\n");
+    printf("\n9) Look up node by index.\n");
+    printf("\n10) Exit\n\n");
     printf("Your Choice: ");
     scanf(" %d", &user_choice);       
 
 
-    /*Create a list SECTION*/
+    /*Create a list Single Node SECTION*/
     
-    if(user_choice == 1) {
-      printf("\nCreate a list with no nodes.\n\n");
-      create_list_no_nodes();      
-    }
-    /*Create a list with a single node SECTION*/
     
-    else if(user_choice == 2) {
-      printf("\nCreate a list with a single node.\n\n");
-      create_list_node();
-    }    
+   if(user_choice == 1) {
+    
+    collect_data();      
+    create_list_node(new_node);
+    } 
+    
+    
+       
     /*Insert node at front SECTION*/
     
-    else if(user_choice == 3) {
+    else if(user_choice == 2) {
       printf("\n\n\nInsert node at front of list.\n");
       insert_front();
     }    
     /*Insert node in middle of list SECTION*/ 
        
-    else if(user_choice == 4){
+    else if(user_choice == 3){
     printf("\n\n\nInsert node in middle of list.\n");
     insert_middle();
     }      
     /*Insert node at end of list SECTION*/
 
-    else if(user_choice == 5){
+    else if(user_choice == 4){
       printf("\n\n\nInsert node at end of list.\n");
       insert_end();
       }
     /*Delete first node of list SECTION*/
 
-  else if(user_choice == 6){
+  else if(user_choice == 5){
     printf("\n\n\nDelete first node of list.\n");
     delete_front();
     }
     /*Delete middle node of list SECTION*/
 
-    else if(user_choice == 7){
+    else if(user_choice == 6){
       printf("\n\n\nDelete middle node of list.\n");
       delete_middle();
       }
     /*Delete node at end of list SECTION*/
 
-    else if(user_choice == 8){
+    else if(user_choice == 7){
       printf("\n\n\nDelete node at end of list.\n");
       delete_end();
       }
     /*Traverse list SECTION*/
 
-    else if(user_choice == 9){
+    else if(user_choice == 8){
       printf("\n\n\nTraverse list.\n");
       traverse();
       }
     /*Look up node by index SECTION*/
 
-    else if(user_choice == 10){
+    else if(user_choice == 9){
       printf("\n\n\nLook up node by index.\n");
       look_up_by_index();
        }  
          
     /*END CHOICE SECTION*/
-    else if(user_choice == 11){
+    else if(user_choice == 10){
       printf("\nThank you for using my program!!\n\n");
       return 0;
     }
     else{
       printf("Invalid Choice");
     }
-  } while (user_choice != 11); 
+  } while (user_choice != 10); 
   
   return 0;
   }
     
-void create_list_no_nodes()
-{
 
-}
   
-void create_list_node()
+void create_list_node(new_node)
 {
 struct StudentInfo *new_node, *current;
 new_node = (struct StudentInfo *)malloc(sizeof(struct StudentInfo));
 
-printf("/nEnter Student's First Name: ");
+if (new_node == (struct StudentInfo *) NULL){
+  printf("\nFailed to Allocate StudentInfo array\n");
+  exit(1);
+  }
+  
+/*printf("/nEnter Student's First Name: ");
 scanf("%s", &new_node->first_name);
 printf("/nEnter Student's Last Name: ");
 scanf("%s", &new_node->last_name);
@@ -158,7 +160,7 @@ printf("/nEnter Student's PU ID: ");
 scanf("%s", &new_node->ID);
 printf("/nEnter Student's Age: ");
 scanf("%s", &new_node->age);
-new_node->nextaddr=NULL;
+new_node->nextaddr=NULL;*/
 
 if (start==NULL)
 {
@@ -173,12 +175,47 @@ current=new_node;
 display_list(start);
 }  
 
+/* Collect User Data Function */
+
+void collect_data(){
+
+struct StudentInfo new_node;
+    char fname[15];
+    char lname[25];
+    char pid[10];
+    char stuage[2];
+    
+      printf("\nCreate a list with a single node.\n\n");
+      printf("\nEnter Student's First Name: ");
+      gets(fname); 
+      strcpy(new_node.first_name, fname);
+           
+      printf("\nEnter Student's Last Name: ");
+      gets(lname);
+      strcpy(new_node.last_name, lname);
+           
+      printf("\nEnter Student's PU ID: ");
+      gets(pid);
+      strcpy(new_node.ID, pid);
+           
+      printf("\nEnter Student's Age: ");
+      gets(stuage);
+      strcpy(new_node.age, stuage); 
+      
+      return new_node;
+}
+
 
 void display_list(struct StudentInfo *contents)
 {
   while (contents !=NULL)
   {
-    printf("%-30s %-20s %-10s %s \n" contents->first_name, contents->last_name, contents->ID, contents->age);
+  char fname[15];
+    char lname[25];
+    char pid[10];
+    char stuage[2];
+    
+    printf("%-30s %-20s %-10s %s \n", contents->first_name, contents->last_name, contents->ID, contents->age);
     contents = contents->nextaddr;
     }
     }  
